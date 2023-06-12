b36b266802450859b0d8905c2a07c53e6853405b
Lakshya000007 <lakshya719@gmail.com>
2023-06-12 11:02:45 +0530

    Form Added for Add to cart Button

--- src/components/Header/Meals/MealShow.jsx
+++ src/components/Header/Meals/MealShow.jsx
@@ -3,21 +3,52 @@ import "./MealShow.css";
 const MealShow = ({ itemList }) => {
   return (
     <>
-      <center>
+      <center style={{ fontSize: "18px" }}>
         <div className="item-center">
           {itemList.map((item) => {
             return (
               <>
-                <div id="self" style={{ whiteSpace: "nowrap" }}>
-                  <b>{item.itemName}</b>
-                </div>
-                <div id="self">
-                  <div style={{ color: "grey", whiteSpace: "nowrap" }}>
-                    {item.itemDescription}
+                <div className="item">
+                  <div className="left-side">
+                    <div id="self" style={{ whiteSpace: "nowrap" }}>
+                      <b>{item.itemName}</b>
+                    </div>
+                    <div id="self">
+                      <div style={{ color: "grey", whiteSpace: "nowrap" }}>
+                        {item.itemDescription}
+                      </div>
+                      <div id="self">
+                        <b style={{ color: "red" }}>${item.cost}</b>
+                      </div>
+                    </div>
                   </div>
-                </div>
-                <div id="self">
-                  <b style={{ color: "red" }}>${item.cost}</b>
+                  <div className="right-side">
+                    <div className="amount">
+                      <label htmlFor="amt">Amount</label>
+                      <input
+                        type="number"
+                        id="amt"
+                        style={{
+                          width: "50px",
+                          marginLeft: "10px",
+                          marginBottom: "10px",
+                        }}
+                      />
+                    </div>
+                    <div className="add">
+                      <button
+                        style={{
+                          color: "white",
+                          marginLeft: "50px",
+                          width: "75px",
+                          padding: "2px",
+                          marginBottom: "5px",
+                        }}
+                      >
+                        + Add
+                      </button>
+                    </div>
+                  </div>
                 </div>
                 <hr />
               </>
